ef16dd5f4f2082270a05d40eec9bfb25a6881ad8
Irving <moixllik@outlook.com>
2023-11-07 07:42:48 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog
+
+Blog escrito en Rust con MongoDB, acceso a GitHub API y desplegado en Fly con Docker
---
+++ public/js/ads.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', _ => {
+    setTimeout(_ => {
+        const $script = document.createElement('script');
+        $script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1552191203398756';
+        document.head.appendChild($script);
+    }, 1000);
+});
